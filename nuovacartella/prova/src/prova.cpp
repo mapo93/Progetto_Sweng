351f931f26af4d710052daed2448c7729761abc8
Luca Mapelli <luca.mapelli93@gmail.com>
2016-03-18 00:14:29 +0100

    inserimento su github

---
+++ nuovacartella/prova/src/prova.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : prova.cpp
+// Author      : luca
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
